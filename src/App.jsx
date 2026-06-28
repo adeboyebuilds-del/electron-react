7174e17420709fd032be663c956acf3b834ae47a
adeboyebuilds-del <adeboyebuilds@gmail.com>
2026-06-28 02:21:42 +0100

    feat: added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 export default function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
       <h1 className="text-2xl font-bold text-blue-500">Welcome to Electron + React! 🚀</h1>
       <p>Your Vite-powered desktop interface is ready.</p>
+      <Button>Click</Button>
     </div>
   );
 }
--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+// These two lines calculate the path to your current directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),], // <-- Add the react plugin here
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      // This tells Vite: "Whenever you see @/, look in the src folder"
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
